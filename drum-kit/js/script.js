fdd5634f6789114ad32b9a284db3d93f7bed8a6e
andreiyusupau <nevermind.qqq@gmail.com>
2021-01-22 23:57:00 +0300

    Add fading effect.

--- drum-kit/js/script.js
+++ drum-kit/js/script.js
@@ -1,12 +1,21 @@
-window.addEventListener('keydown', (e) => {
-    const audio=document.querySelector(`audio[data-key="${e.key}"]`);
-    const key=document.querySelector(`.key[data-key="${e.key}"]`);
-    if(audio) {
-        audio.currentTime=0;
+window.addEventListener('keydown',playSound);
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
+    if (audio) {
+        audio.currentTime = 0;
         audio.play();
         key.classList.add('playing');
-        setTimeout(()=>{
-
-        });
     }
-});
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if (e.propertyName === 'transform') {
+        this.classList.remove('playing');
+    }
+}
+
