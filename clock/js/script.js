f5e8e23ba175ab790731b0e1afd7a515ae82da37
andreiyusupau <nevermind.qqq@gmail.com>
2021-01-24 13:33:56 +0300

    Remove offset.

--- clock/js/script.js
+++ clock/js/script.js
@@ -1,29 +1,25 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-const offset=document.querySelector('.hand').style.rotate;
 
 function setDate() {
-    console.log(offset);
-    // const offset = 90;
-
     const now = new Date();
 
     const seconds = now.getSeconds();
     const secondsDegrees = seconds / 60 * 360;
-    turn(secondHand, secondsDegrees, offset);
+    turn(secondHand, secondsDegrees);
 
     const minutes = now.getMinutes();
     const minuteDegrees = minutes / 60 * 360;
-    turn(minuteHand, minuteDegrees, offset);
+    turn(minuteHand, minuteDegrees);
 
     const hours = now.getHours();
     const hourDegrees = hours % 12 / 12 * 360 + minuteDegrees / 12;
-    turn(hourHand, hourDegrees, offset);
+    turn(hourHand, hourDegrees);
 }
 
-function turn(hand, degrees, offset) {
-    hand.style.transform = `rotate(${degrees + offset}deg)`;
+function turn(hand, degrees) {
+    hand.style.transform = `rotate(${degrees}deg)`;
 }
 
 setInterval(setDate, 1000);
