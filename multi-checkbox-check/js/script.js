a57eb5a668117cf08bfd33454e61035a415780e3
andreiyusupau <nevermind.qqq@gmail.com>
2021-02-07 17:04:15 +0300

    Add solution.

---
+++ multi-checkbox-check/js/script.js
@@ -0,0 +1,19 @@
+const checkboxes = document.querySelectorAll('.inbox input[type=checkbox]');
+let lastChecked;
+
+function handleClick(e) {
+    if (e.shiftKey && this.checked) {
+        let inBetween = false;
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleClick));
